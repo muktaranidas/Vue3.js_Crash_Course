beb9f20832da73465dd9969e520d88df20231b3a
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-05-10 11:48:28 -0700

    done 1st challange

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  // data: function() {   //ES6 shorthand
+  data() {
+    return {
+      Product: "PC",
+      ProductDescription: "Hi, This is PC. I am a Robot. code is my Life.",
+    };
+  },
+});
